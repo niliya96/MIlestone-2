36598bcd0ffb50573e68e9dca2c680be7d615709
Idany95 <58816983+Idany95@users.noreply.github.com>
2020-01-13 16:46:42 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Milestone2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Milestone2.dir/main.cpp.o"
+  "Milestone2"
+  "Milestone2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Milestone2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Milestone2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Milestone2.
+# This may be replaced when dependencies are built.
