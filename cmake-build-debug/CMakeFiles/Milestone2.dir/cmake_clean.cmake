f3feb71efb9088af76af04c3fb87f3cc9e3b95c1
niliya96 <1niliya1@gmail.com>
2020-01-15 17:29:24 +0200

    commit

--- cmake-build-debug/CMakeFiles/Milestone2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Milestone2.dir/cmake_clean.cmake
@@ -2,7 +2,6 @@ file(REMOVE_RECURSE
   "CMakeFiles/Milestone2.dir/client/MyTestClientHandler.cpp.o"
   "CMakeFiles/Milestone2.dir/main.cpp.o"
   "CMakeFiles/Milestone2.dir/server/MySerialServer.cpp.o"
-  "CMakeFiles/Milestone2.dir/solver/Solver.cpp.o"
   "Milestone2"
   "Milestone2.pdb"
 )
