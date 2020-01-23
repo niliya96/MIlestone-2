549d74d824fde29e0ef708d235fe42b5c2463b83
Idany95 <idanyarchi1@gmail.com>
2020-01-23 13:46:07 +0200

    Fixed CacheManager issues need to add notes and such

---
+++ cmake-build-debug/CMakeFiles/MIlestone_2.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/idan/CLionProjects/MIlestone-2/cacheManager/FileCacheManager.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/cacheManager/FileCacheManager.cpp.o"
+  "/home/idan/CLionProjects/MIlestone-2/client/MyClientHandler.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/client/MyClientHandler.cpp.o"
+  "/home/idan/CLionProjects/MIlestone-2/client/MyTestClientHandler.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/client/MyTestClientHandler.cpp.o"
+  "/home/idan/CLionProjects/MIlestone-2/main.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/main.cpp.o"
+  "/home/idan/CLionProjects/MIlestone-2/searchable/Matrix.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/searchable/Matrix.cpp.o"
+  "/home/idan/CLionProjects/MIlestone-2/searcher/AStar.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/searcher/AStar.cpp.o"
+  "/home/idan/CLionProjects/MIlestone-2/searcher/BestFS.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/searcher/BestFS.cpp.o"
+  "/home/idan/CLionProjects/MIlestone-2/server/MyParallelServer.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/server/MyParallelServer.cpp.o"
+  "/home/idan/CLionProjects/MIlestone-2/server/MySerialServer.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/server/MySerialServer.cpp.o"
+  "/home/idan/CLionProjects/MIlestone-2/solver/StringReverser.cpp" "/home/idan/CLionProjects/MIlestone-2/cmake-build-debug/CMakeFiles/MIlestone_2.dir/solver/StringReverser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../cacheManager"
+  "../client"
+  "../searchable"
+  "../searcher"
+  "../server"
+  "../solver"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MIlestone_2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MIlestone_2.dir/cacheManager/FileCacheManager.cpp.o"
+  "CMakeFiles/MIlestone_2.dir/client/MyClientHandler.cpp.o"
+  "CMakeFiles/MIlestone_2.dir/client/MyTestClientHandler.cpp.o"
+  "CMakeFiles/MIlestone_2.dir/main.cpp.o"
+  "CMakeFiles/MIlestone_2.dir/searchable/Matrix.cpp.o"
+  "CMakeFiles/MIlestone_2.dir/searcher/AStar.cpp.o"
+  "CMakeFiles/MIlestone_2.dir/searcher/BestFS.cpp.o"
+  "CMakeFiles/MIlestone_2.dir/server/MyParallelServer.cpp.o"
+  "CMakeFiles/MIlestone_2.dir/server/MySerialServer.cpp.o"
+  "CMakeFiles/MIlestone_2.dir/solver/StringReverser.cpp.o"
+  "MIlestone_2"
+  "MIlestone_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MIlestone_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
